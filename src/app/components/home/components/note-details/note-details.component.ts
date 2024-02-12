4fd83fa53af46d4e5ecc4d31df0d3224d1721872
diegoinost02 <diegoinostroza00@gmail.com>
2024-02-12 19:42:45 -0300

    Se agrega la interacción de saveChanges y deleteNote

--- src/app/components/home/components/note-details/note-details.component.ts
+++ src/app/components/home/components/note-details/note-details.component.ts
@@ -3,22 +3,56 @@ import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/materia
 import { Note } from '../../../../interfaces/note.model';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoteService } from '../../../../services/note.service';
+import { RequestStatus } from '../../../../interfaces/request-status.model';
 
 @Component({
   selector: 'app-note-details',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule, MatFormField, MatLabel],
+  imports: [MatDialogModule, MatButtonModule, MatFormField, MatLabel, ReactiveFormsModule, FormsModule],
   templateUrl: './note-details.component.html',
   styleUrl: './note-details.component.css'
 })
 export class NoteDetailsComponent {
   
   private dialogRef = inject(MatDialogRef);
+  protected noteData: Note = inject(MAT_DIALOG_DATA);
 
-  noteData: Note = inject(MAT_DIALOG_DATA);
-  noteToEdit: Note = {...this.noteData};
+  private formBuilder = inject(FormBuilder);
+  private noteService = inject(NoteService);
 
-  save() {}
+  statusSaveNote: RequestStatus = 'init';
+  statusDeleteNote: RequestStatus = 'init';
 
-  deleteNote() {}
+  noteForm = this.formBuilder.nonNullable.group({
+    title: [this.noteData.title, []],
+    description: [this.noteData.description, []]
+  })
+
+  saveChanges(): void {
+    if(this.noteForm.value !== this.noteData) {
+      const { title, description } = this.noteForm.getRawValue();
+      
+      this.noteService.updateNote({ ...this.noteData, title, description }).subscribe({
+        next: () =>{
+          this.statusSaveNote = 'success';
+        },
+        error: () => {
+          this.statusSaveNote = 'failed';
+        }
+      })
+    }
+  }
+
+  deleteNote(id: number): void {
+    this.noteService.deleteNote(id).subscribe({
+      next: () => {
+        this.statusDeleteNote = 'success';
+      },
+      error: () => {
+        this.statusDeleteNote = 'failed';
+      }
+    })
+  }
 }
