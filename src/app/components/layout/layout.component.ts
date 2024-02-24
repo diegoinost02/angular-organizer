9c0e03c3d10b9d2a9bc4446fca9908f93c8152ed
diegoinost02 <diegoinostroza00@gmail.com>
2024-02-23 21:38:58 -0300

    Se agrega logout automático en caso de error

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { FolderService } from '../../services/folder.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
@@ -14,16 +15,23 @@ export class LayoutComponent implements OnInit{
   private route = inject(ActivatedRoute);
   private userService = inject(UserService);
   private folderService = inject(FolderService);
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const username = params['username']; 
-      this.userService.getProfile(username).subscribe( user => {
+      this.userService.getProfile(username)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (user) => {
           if(user) {
             this.folderService.getUserFolders(user.id).subscribe();
           }
+        },
+        error: () => {
+            this.userService.logout();
         }
-      )
+      })
     })
   }
 }
